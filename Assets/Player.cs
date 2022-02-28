22a108e03cabbadefe0a5010021ffbe4329e8592
Nina Wójcik <neenawu@gmail.com>
2022-02-28 17:39:38 +0100

    Dodanie myproject

---
+++ Assets/Player.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float Szybkosæ = 2.5f;
+    public CharacterController controller;
+    public Vector3 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+;
+        Debug.Log(Input.GetAxis("Horizontal"));
+
+        movement.z = -Input.GetAxis("Horizontal");
+
+
+        movement.x = Input.GetAxis("Vertical");
+
+        if (controller.isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            
+
+            {
+                movement.y = 3f;
+            }
+        }
+        else
+        {
+            movement.y -= 0.01f;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(movement * Time.fixedDeltaTime * Szybkosæ);
+        
+       
+    }
+}
